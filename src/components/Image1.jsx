5c228371e5f00f211b012768398b0e117867978a
SiddiqAM1 <137751069+SiddiqAM1@users.noreply.github.com>
2023-08-06 13:24:58 +0500

    Update Image1.jsx

--- src/components/Image1.jsx
+++ src/components/Image1.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { motion } from "framer-motion";
 import { fadeIn, slideIn, staggerContainer } from ".././utils/motion";
 
+import css from "./Hero/Hero.module.scss";
+
 const YourComponent = () => {
   return (
     <motion.div
