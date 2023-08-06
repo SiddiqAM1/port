37496e05c085944f7ed093353ef5eb00cdf8f2e1
SiddiqAM1 <137751069+SiddiqAM1@users.noreply.github.com>
2023-08-06 13:08:31 +0500

    Update Image1.jsx

--- src/components/Image1.jsx
+++ src/components/Image1.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './YourComponent.css'; // Import your CSS file
+import Proc from './SiddiqImage.jpg'
 
 function YourComponent() {
   return (
     <div className="hero-image">
-      <img src='./SiddiqImage.jpg' alt="Your Image" />
+      <img src={Proc} alt="Your Image" />
     </div>
   );
 }
